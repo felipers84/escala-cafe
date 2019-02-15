445ab3d18381e44294b2ad029900c7754cf1a81a
Felipe Campos <fl.campos@tjro.jus.br>
2019-02-15 12:41:13 -0400

    Script de docker-run configurado para auto-iniciar container no restart do SO

--- docker-run.sh
+++ docker-run.sh
@@ -1,2 +1,2 @@
 docker rm --force nginx-escala-cafe
-docker run --name nginx-escala-cafe -v "$PWD"/docs:/usr/share/nginx/html:ro -d  -p 1979:80 nginx
+docker run --name nginx-escala-cafe --restart always -v "$PWD"/docs:/usr/share/nginx/html:ro -d  -p 1979:80 nginx
