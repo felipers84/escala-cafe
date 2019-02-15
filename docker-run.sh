63dbaf0b448ed726426edbfff11e8c05dcb403a1
Felipe Campos <fl.campos@tjro.jus.br>
2019-02-15 12:17:27 -0400

    Estrutura /docs para publicaçao no GitHub pages

--- docker-run.sh
+++ docker-run.sh
@@ -1,2 +1,2 @@
 docker rm --force nginx-escala-cafe
-docker run --name nginx-escala-cafe -v "$PWD"/dist/escala-cafe:/usr/share/nginx/html:ro -d  -p 1979:80 nginx
+docker run --name nginx-escala-cafe -v "$PWD"/docs:/usr/share/nginx/html:ro -d  -p 1979:80 nginx
