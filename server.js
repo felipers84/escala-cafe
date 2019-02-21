c702eda29a668904620e9291317018dc1a5b673a
Felipe Campos <fl.campos@tjro.jus.br>
2019-02-21 08:41:49 -0400

    Mais correçoes index.js do serviço express

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const app = express();
 const path = require('path');
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/dist'));
-app.get("/*all", (req, res) => res.sendFile(path.join(__dirname + '/dist/index.html')));
+//app.use(express.static(__dirname + '/dist'));
+app.get("/", (req, res) => res.sendFile(path.join(__dirname + '/dist/index.html')));
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
