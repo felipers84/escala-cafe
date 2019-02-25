e988f3fa8c1e41125763ed82aac3a67b5a23be46
Felipe Campos <fl.campos@tjro.jus.br>
2019-02-25 17:25:28 -0400

    Correçoes no server.js 2

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+app.use(express.static(__dirname + '/docs'));
+
 app.get("/", (req, res) => res.sendFile(path.join(__dirname + '/docs/index.html')));
 
 app.listen(process.env.PORT || 8080);
